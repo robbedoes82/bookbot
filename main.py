8e32c38ddf0497fc18ce7acf23c269115b0e97d3
Robin Andrianne <robbedoes@gmail.com>
2025-08-03 15:57:37 +0200

    added character_count function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,6 +10,7 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = word_count(text)
     print(f"{num_words} words found in the document")
+    print(character_count(text))
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def word_count(string):
     wc = len(string.split())
     return wc
+
+def character_count(string):
+    character_count = {}
+    for letter in string.lower():
+        # letter = letter.lower()
+        if letter not in character_count:
+            character_count[letter] = 1
+        else:
+            character_count[letter] += 1
+    return character_count    
+
+# test
+# print(character_count("Hallo ik ben Robin, ik ben 42 jaar oud."))
