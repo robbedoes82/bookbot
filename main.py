06036e9ae3f0ecce29b202bcbb1eb1b03ebb1413
Robin Andrianne <robbedoes@gmail.com>
2025-08-05 15:51:03 +0200

    added option to supply book path from command line

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import sys
 from stats import word_count, character_count, sort_character_list
 
 def get_book_text(filepath):
@@ -7,7 +9,11 @@ def get_book_text(filepath):
 
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    # filepath = "books/frankenstein.txt"
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     num_words = word_count(text)
     character_dic = character_count(text)
@@ -24,44 +30,3 @@ def main():
 
 main()
 
-
-
-'''
-============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
------------ Word Count ----------
-Found 75767 total words
---------- Character Count -------
-e: 44538
-t: 29493
-a: 25894
-o: 24494
-i: 23927
-n: 23643
-s: 20360
-r: 20079
-h: 19176
-d: 16318
-l: 12306
-m: 10206
-u: 10111
-c: 9011
-f: 8451
-y: 7756
-w: 7450
-p: 5952
-g: 5795
-b: 4868
-v: 3737
-k: 1661
-x: 691
-j: 497
-q: 325
-z: 235
-æ: 28
-â: 8
-ê: 7
-ë: 2
-ô: 1
-============= END ===============
-'''
